1650c44114716ce8697a898498084aacfa6c5b46
Harsha <harsha1127achu@gmail.com>
2026-05-27 03:00:59 +0530

    small tweak in login ui

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,7 +4,8 @@ import { LoginForm } from "@/src/components/auth/login-form"
 
 export default function RegisterPage() {
   return (
-      <div className="flex w-full max-w-sm flex-col gap-4">
+    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
         <a href="#" className="flex items-center gap-2 self-center font-medium">
           <div className="flex size-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
             <Briefcase className="size-4" />
@@ -13,5 +14,6 @@ export default function RegisterPage() {
         </a>
         <LoginForm />
       </div>
+    </div>
   )
 }
