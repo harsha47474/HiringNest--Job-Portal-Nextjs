9d4cf8c6c29a851dc5aca531e498043d1ebfd18f
Harsha <harsha1127achu@gmail.com>
2026-05-29 20:21:28 +0530

    change of file location

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Toaster } from "@/components/ui/sonner"
+import { Toaster } from "@/src/components/ui/sonner"
 
 const inter = Inter({
   variable: "--font-inter",
